28f8ee83befe634633710b0b29491b067e975bb7
Dl0rd <124901949+Dl0rd@users.noreply.github.com>
2023-10-22 22:05:01 +0700

    v1.0.2

--- application_logic.py
+++ application_logic.py
@@ -1,5 +1,50 @@
 import flet as f
 
+
+class Table(f.UserControl):
+    def __init__(self,page):
+        super().__init__()
+        self.page = page
+        self.start_inscription = f.Container(
+            content=f.Column(
+                controls=[
+                    f.Container(
+                        content=f.Text('Добавить расписание',text_align=f.alignment.center),
+                        alignment=f.alignment.center,
+                    ),
+                    f.Container(
+                        content=f.FloatingActionButton(
+                            icon=f.icons.ADD,
+                            on_click=self.clear_all_table
+                            ),
+                        alignment=f.alignment.center,
+                    )
+                ],
+                alignment=f.MainAxisAlignment.CENTER,
+                height=500,
+            ),
+        )
+    def build(self):
+        self.layuot = f.Stack(controls=[self.start_inscription])
+        return self.layuot
+    def create_col(self,*e):
+        # dlg_modal = f.AlertDialog(
+        #         modal=True,
+        #         title=f.Text("Please confirm"),
+        #         content=f.Text("Do you really want to delete all those files?"),
+        #         actions=[
+        #             f.TextButton("Yes", on_click=close_dlg),
+        #             f.TextButton("No", on_click=close_dlg),
+        #         ],
+        #         actions_alignment=ft.MainAxisAlignment.END,
+        #         on_dismiss=lambda e: print("Modal dialog dismissed!"),
+        #     )
+        ...
+    def clear_all_table(self,e):
+        self.layuot.clean()
+        self.page.update()
+
+
 def main(page: f.Page):
 
     lessons = f.Dropdown(
@@ -40,55 +85,13 @@ def main(page: f.Page):
 
     
 
-    layout = f.Container(padding=10)
-    # def on_change_col_field(e):
-    #     if e.control.value:
-    #         b1.disabled = False
-    #     else:
-    #         b1.disabled = True
-    #     page.update()
-        
-    # def add(e):
-    #     if dd.value == None or dd.value == 'New col':
-    #         table.columns.append(f.DataColumn(f.Text(f1.value)))
-    #         dd.options.append(f.dropdown.Option(f1.value))
-            
-    #     else:
-    #         table.rows.append(
-    #             f.DataRow(
-    #                 cells=[
-    #                     f.DataCell(f.Text(f1.value)),
-    #                 ],
-    #             ),
-    #         )
-    #     f1.value = ''
-    #     b1.disabled = True
-    #     page.update()
-
-
-    # dd = f.Dropdown(
-    #     width=100,
-    #     options=[f.dropdown.Option("New col")],
-    # )
-    # table = f.DataTable()
-    # f1 = f.TextField(label="name obj",on_change=on_change_col_field,)
-    # b1 = f.FilledButton(text='add obj', on_click=add,disabled=True)
-    # page.add(
-    #     f.Row(controls=[
-    #     dd,
-    #     f1,
-    #     b1,
-    #     ]),
-    #     table
-    # )
+    layout = Table(page)
     def open_dlg(e):
         page.dialog = dlg
         dlg.open = True
         page.update()
 
-    page.add(
-        f.ElevatedButton("Open dialog", on_click=open_dlg)
-        )
-
+    page.add(f.ElevatedButton("Open dialog", on_click=open_dlg),layout)
+    
 
 f.app(target=main)
